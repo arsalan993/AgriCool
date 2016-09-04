e35004b4e26ac4583784791b1d56c97f4d72ae55
arsalan993 <arslan993@hotmail.com>
2016-09-04 22:14:53 +0500

    check
    text

---
+++ init.lua
@@ -0,0 +1,19 @@
+
+tmr.alarm(1, 1000, 1, function()
+
+print("value high")
+
+
+tmr.alarm(2, 900, 1, function()
+
+print("value low")
+
+tmr.alarm(3, 800, 1, function()
+
+print("value low")
+end)
+
+end)
+
+
+end)
